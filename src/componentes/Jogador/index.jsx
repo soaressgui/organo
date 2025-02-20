076b942e2887cd58b7bde724bd58ae675f4f36b5
Guilherme <guilhermehenriquels@hotmail.com>
2025-02-20 20:11:11 -0300

    consertando bug no card do jogador

--- src/componentes/Jogador/index.jsx
+++ src/componentes/Jogador/index.jsx
@@ -1,14 +1,14 @@
 import "./Jogador.css";
 
-function Jogador(props, corDeFundo) {
+function Jogador({imagem, nome, cargo, corDeFundo}) {
   return (
     <div className="jogador">
       <div className="cabecalho" style={{backgroundColor: corDeFundo}}>
-        <img src={props.imagem} alt={props.nome} />
+        <img src={imagem} alt={nome} />
       </div>
       <div className="rodape">
-        <h4>{props.nome}</h4>
-        <h5>{props.cargo}</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   )
